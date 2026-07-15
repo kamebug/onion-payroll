dcc214d904431bbc33df22d45121fc85780ec6ba
kamebug <kamebug@gmail.com>
2026-07-15 10:48:04 +0900

    Deploy 2607151043

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -51,7 +51,7 @@ if (flet.noCdn) {
 _flutter.loader.load({
     config: flutterConfig,
     serviceWorkerSettings: {
-        serviceWorkerVersion: "4210937860" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+        serviceWorkerVersion: "1592575057" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const engine = await engineInitializer.initializeEngine(flutterConfig);
